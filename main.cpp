e467b3d55023eaffb9859645c4f0304ec1ece865
amandacruzz <73866820+amandacruzz@users.noreply.github.com>
2022-04-19 18:36:10 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <unordered_map>
 #include <vector>
 #include <map>
+#include <string.h>
+#include <sstream>
 using namespace std;
 
 class unOrderedMaps
@@ -10,15 +12,17 @@ class unOrderedMaps
 public:
     ifstream dataset;
 
-    // Map with key = country and values = news article title
-    unordered_map<string, string> countryNews;
-    // Vector to hold titles for data of each country
-    vector<string> titles1;  // U.S. news data
-    vector<string> titles2;
-    vector<string> titles3;
+    // Map with key = title, value = number of occurrences
+    unordered_map<string, int> Arab;
+    unordered_map<string, int> China;
+    unordered_map<string, int> UnitedStates;
+    // keep counter for number of articles in each country
+    int ArabCount = 0;
+    int ChinaCount = 0;
+    int UnitedStatesCount = 0;
 
     // Methods
-    string getTitle(string line);
+    void insertTitle(string line);
     vector<int> indexCalculator();
 };
 
@@ -27,251 +31,183 @@ class orderedMapCalc
 public:
     ifstream dataset;
 
-    // Map with key = country and values = news article title
-    map<string, string> countryNews;
-    // Vector to hold titles for data of each country
-    vector<string> titles1;  // U.S. news data
-    vector<string> titles2;  // TODO: get country data
-    vector<string> titles3;  // TODO: get country data
+    // Map with key = title, value = number of occurrences
+    map<string, int> Arab;
+    map<string, int> China;
+    map<string, int> UnitedStates;
+    // keep counter for number of articles in each country
+    int ArabCount = 0;
+    int ChinaCount = 0;
+    int UnitedStatesCount = 0;
 
     // Methods
-    string getTitleInsert(string line);
+    void getTitleInsert(string line);
     vector<int> calculateIndex();
 };
 
 // method to parse title and insert into map
-string unOrderedMaps::getTitle(string countryData)
+void unOrderedMaps::insertTitle(string countryData)
 {
-    if (countryData == "articles1.csv"){
+    if (countryData == "news-week-17aug24.csv") {
         dataset.open(countryData);
         string input, line;
         int i = 0;
         getline(dataset, line);
-        cout << "test " << i << ": " << line <<  endl;
-        while(getline(dataset, line)){
+        // cout << "test " << i << ": " << line << endl;
+        while (getline(dataset, line)) {
             i++;
-            cout << "test " << i << ": " << line << endl;
-        }
-        //TODO: parse string
-        // add to vector
-
-        // insert into map using vector
-        for (int i = 0; i < titles1.size(); i++){
-            countryNews.insert(pair<string, string>(countryData,titles1[i]));
-        }
-    }
-    else if (countryData == "tbd1"){    //FIXME:
-        dataset.open(countryData);
-        string input, line;
-        int i = 0;
-        getline(dataset, line);
-        cout << "test " << i << ": " << line <<  endl;
-        while(getline(dataset, line)){
-            i++;
-            cout << "test " << i << ": " << line << endl;
-        }
-        //TODO: parse string
-        // add to vector
-
-        // insert into map using vector
-        for (int i = 0; i < titles2.size(); i++){
-            countryNews.insert(pair<string, string>(countryData,titles2[i]));
-        }
-    }
-    else if (countryData == "tbd2"){    //FIXME:
-        dataset.open(countryData);
-        string input, line;
-        int i = 0;
-        getline(dataset, line);
-        cout << "test " << i << ": " << line <<  endl;
-        while(getline(dataset, line)){
-            i++;
-            cout << "test " << i << ": " << line << endl;
-        }
-        //TODO: parse string
-        // add to vector
-
-        // insert into map using vector
-        for (int i = 0; i < titles3.size(); i++){
-            countryNews.insert(pair<string, string>(countryData,titles3[i]));
+            // cout << "test " << i << ": " << line << endl;
+            //FIXME: if(first char == Arab char)
+            // insert into Arab map
+            ArabCount++;
+            //FIXME: if(first char == Chinese char)
+            // insert into China map
+            ChinaCount++;
+            //FIXME: if(first char == US char)
+            // insert US map
+            UnitedStatesCount++;
         }
     }
 }
-// method to calculate prosperity index: add up words of first 100,000 articles and divide it by 100,000
+
+// method to calculate prosperity index: add up num of words in articles of each country / num of articles in each country
 vector<int> unOrderedMaps::indexCalculator()
 {
     vector<int>prosperityIndex;
 
-    // TODO: count the number of words in vector of country 1
+    // TODO: count the number of words in vector of Arab
     int totalWords1 = 0;
-    for (int i = 0; i < titles1.size(); i++){
+//    for (int i = 0; i < titles1.size(); i++){
+//
+//    }
+    prosperityIndex[0] = totalWords1 / ArabCount;
 
-    }
-    prosperityIndex[0] = totalWords1 / 30,000;  //FIXME: adjust 30,000
-
-    // TODO: count the number of words in vector of country 1
+    // TODO: count the number of words in vector of China
     int totalWords2 = 0;
-    for (int i = 0; i < titles2.size(); i++){
+//    for (int i = 0; i < titles2.size(); i++){
+//
+//    }
+    prosperityIndex[1] = totalWords2 / ChinaCount;
 
-    }
-    prosperityIndex[1] = totalWords2 / 30,000;  //FIXME: adjust 30,000
-
-    // TODO: count the number of words in vector of country 1
+    // TODO: count the number of words in vector of US
     int totalWords3 = 0;
-    for (int i = 0; i < titles3.size(); i++){
-
-    }
-    prosperityIndex[2] = totalWords3 / 30,000;  //FIXME: adjust 30,000
+//    for (int i = 0; i < titles3.size(); i++){
+//
+//    }
+    prosperityIndex[2] = totalWords3 / UnitedStatesCount;
 
     return prosperityIndex;
 }
+
 // method to parse title and insert into map
-string orderedMapCalc::getTitleInsert(string countryData) {
-    if (countryData == "articles1.csv"){
-        dataset.open(countryData);
-        string input, line;
-        int i = 0;
-        getline(dataset, line);
-        cout << "test " << i << ": " << line <<  endl;
-        while(getline(dataset, line)){
-            i++;
-            cout << "test " << i << ": " << line << endl;
-        }
-        //TODO: parse string
-        // add to vector
+void orderedMapCalc::getTitleInsert(string countryData) {
 
-        // insert into map using vector
-        for (int i = 0; i < titles1.size(); i++){
-            countryNews.insert(pair<string, string>(countryData,titles1[i]));
-        }
-    }
-    else if (countryData == "tbd1"){    //FIXME:
+    if (countryData == "news-week-17aug24.csv") {
         dataset.open(countryData);
-        string input, line;
-        int i = 0;
+        string input, line;int i = 0;
         getline(dataset, line);
-        cout << "test " << i << ": " << line <<  endl;
-        while(getline(dataset, line)){
+        // cout << "test " << i << ": " << line << endl;
+        while (getline(dataset, line)) {
             i++;
-            cout << "test " << i << ": " << line << endl;
-        }
-        //TODO: parse string
-        // add to vector
+            // cout << "test " << i << ": " << line << endl;
 
-        // insert into map using vector
-        for (int i = 0; i < titles2.size(); i++){
-            countryNews.insert(pair<string, string>(countryData,titles2[i]));
-        }
-    }
-    else if (countryData == "tbd2"){    //FIXME:
-        dataset.open(countryData);
-        string input, line;
-        int i = 0;
-        getline(dataset, line);
-        cout << "test " << i << ": " << line <<  endl;
-        while(getline(dataset, line)){
-            i++;
-            cout << "test " << i << ": " << line << endl;
-        }
-        //TODO: parse string
-        // add to vector
+            //FIXME: if(first char == Arab char)
+            // insert into Arab map
+            ArabCount++;
 
-        // insert into map using vector
-        for (int i = 0; i < titles3.size(); i++){
-            countryNews.insert(pair<string, string>(countryData,titles3[i]));
+            //FIXME: if(first char == Chinese char)
+            // insert into China map
+            ChinaCount++;
+
+            //FIXME: if(first char == US char)
+            // insert US map
+            UnitedStatesCount++;
         }
     }
 }
+
 // method to calculate prosperity index: add up words of first 100,000 articles and divide it by 100,000
 vector<int> orderedMapCalc::calculateIndex() {
     vector<int>prosperityIndex;
 
-    // TODO: count the number of words in vector of country 1
+    // TODO: count the number of words in vector of Arab
     int totalWords1 = 0;
-    for (int i = 0; i < titles1.size(); i++){
+//   for (int i = 0; i < titles1.size(); i++){
+//
+//   }
+    prosperityIndex[0] = totalWords1 / ArabCount;
 
-    }
-    prosperityIndex[0] = totalWords1 / 30,000;  //FIXME: adjust 30,000
-
-    // TODO: count the number of words in vector of country 1
+    // TODO: count the number of words in vector of China
     int totalWords2 = 0;
-    for (int i = 0; i < titles2.size(); i++){
+//   for (int i = 0; i < titles2.size(); i++){
+//
+//   }
+    prosperityIndex[1] = totalWords2 / ChinaCount;
 
-    }
-    prosperityIndex[1] = totalWords2 / 30,000;  //FIXME: adjust 30,000
-
-    // TODO: count the number of words in vector of country 1
+    // TODO: count the number of words in vector of US
     int totalWords3 = 0;
-    for (int i = 0; i < titles3.size(); i++){
-
-    }
-    prosperityIndex[2] = totalWords3 / 30,000;  //FIXME: adjust 30,000
+//   for (int i = 0; i < titles3.size(); i++){
+//
+//   }
+    prosperityIndex[2] = totalWords3 / UnitedStatesCount;
 
     return prosperityIndex;
 }
 
 
+int main()
+{
+    ifstream dataset;
+    string file = "news-week-17aug24.csv";
+    dataset.open(file);
+    string input, line;
+    int i = 0;
+    getline(dataset, line);
+    cout << "test " << i << ": " << line <<  endl;
+    while(getline(dataset, line)){
+        i++;
+        cout << "test " << i << ": " << line << endl;
+    }
 
-int main() {
     unOrderedMaps prosperityCalculator;
     orderedMapCalc prosperityCalc;
 
-    // Parse first country data and insert into map
-    string file1 = "articles1.csv";
-    prosperityCalculator.getTitle(file1);
+    // UNORDERED MAPS: Parse data and insert into map for each key
+    string file1 = "news-week-17aug24.csv";
+    prosperityCalculator.insertTitle(file1);
+
+    // Reset stream for another pass
+    prosperityCalculator.dataset.clear();
+    prosperityCalculator.dataset.seekg(0);
+    prosperityCalc.dataset.clear();
+    prosperityCalc.dataset.seekg(0);
+
+    // ORDERED MAPS: Parse data and insert into map for each key
     prosperityCalc.getTitleInsert(file1);
 
-    // Reset stream for another pass
-    prosperityCalculator.dataset.clear();
-    prosperityCalculator.dataset.seekg(0);
-
-    prosperityCalc.dataset.clear();
-    prosperityCalc.dataset.seekg(0);
-
-    // Parse second country data and insert into map
-    string file2 = "CHANGE";   //FIXME
-    prosperityCalculator.getTitle(file2);
-    prosperityCalc.getTitleInsert(file2);
-
-    // Reset stream for another pass
-    prosperityCalculator.dataset.clear();
-    prosperityCalculator.dataset.seekg(0);
-    prosperityCalc.dataset.clear();
-    prosperityCalc.dataset.seekg(0);
-
-    // Parse third country data and insert into map
-    string file3 = "CHANGE";   //FIXME
-    prosperityCalculator.getTitle(file3);
-    prosperityCalc.getTitleInsert(file3);
-
-    // Calculate the prosperity index of three countries
+    // UNORDERED MAP: Calculate the prosperity index of three countries
     vector<int> unorderedIdx;
     unorderedIdx = prosperityCalculator.indexCalculator();
     int idx1, idx2, idx3;
     idx1 = unorderedIdx[0];
     idx2 = unorderedIdx[1];
     idx3 = unorderedIdx[2];
-
     // Display data
-    cout << "The prosperity index of the United States is: " << idx1 << endl;
-    cout << "The prosperity index of the CHANGE country is: " << idx2 << endl;
-    cout << "The prosperity index of the CHANGE country is: " << idx3 << endl;
+    cout << "The prosperity index of the United States using unordered map is: " << idx1 << endl;
+    cout << "The prosperity index of the CHANGE country using unordered map is: " << idx2 << endl;
+    cout << "The prosperity index of the CHANGE country using unordered map is: " << idx3 << endl;
 
-
-
-
-    // Calculate the prosperity index of three countries
+    // ORDERED MAP: Calculate the prosperity index of three countries
     vector<int> index;
     index = prosperityCalc.calculateIndex();
     int index1, index2, index3;
     index1 = index[0];
     index2 = index[1];
     index3 = index[2];
-
     // Display data
-    cout << "The prosperity index of the United States is: " << index1 << endl;
-    cout << "The prosperity index of the TBD is: " << index2 << endl;
-    cout << "The prosperity index of the TBD is: " << index3 << endl;
+    cout << "The prosperity index of the United States using ordered map is: " << index1 << endl;
+    cout << "The prosperity index of the TBD using ordered map is: " << index2 << endl;
+    cout << "The prosperity index of the TBD using ordered map is: " << index3 << endl;
 
     return 0;
 }
